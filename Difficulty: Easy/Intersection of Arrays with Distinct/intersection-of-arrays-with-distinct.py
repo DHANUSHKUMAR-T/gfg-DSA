1e87887e5c7cb3f090401e14b468510be4b5527a
DHANUSHKUMAR T <t.dhanu2216@gmail.com>
2025-12-19 14:01:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Arrays with Distinct/intersection-of-arrays-with-distinct.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersectSize(self,a, b):
+        count=0
+        x = a+b
+        a = max(x)
+        hash = [0]*a
+        for i in x:
+            hash[i-1]+=1
+        
+        for i in range(len(hash)):
+            if hash[i]==2:
+                count+=1
+        return count
